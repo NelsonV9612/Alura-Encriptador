474330e0dc3c5584f83d3efdc69b0a3e89ce9c26
Nelson Valdés <99925372+NelsonV9612@users.noreply.github.com>
2022-11-04 19:33:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ function encrypt(encryptString) {
     for (let i = 0; i < matriz.length; i++) {
         if (encryptString.includes(matriz[i][0])) {
            encryptString=encryptString.replaceAll(matriz[i][0],matriz[i][1]);
-           /* aca hago que me reemplace la letra recorriendo el texto ingresado 0 seria a y 1 seria ai */
         }
     }
     return encryptString;
@@ -25,30 +24,29 @@ function unencrypt(unencryptString) {
     for (let i = 0; i < matriz.length; i++) {
         if (unencryptString.includes(matriz[i][1])) {
            unencryptString=unencryptString.replaceAll(matriz[i][1],matriz[i][0]);
-           /* aca hago que me reemplace la letra recorriendo el texto ingresado 0 seria a y 1 seria ai */
+           
         }
     }
     return unencryptString;
 }
 function btnEncriptar() {
-    /* aca guardo dentro de encryptText el valor encriptado una vez se ejecuta la funcion encrypt 
-    la cual le paso el valor del inputText que traigo de html */
+    
     let encryptText=encrypt(inputText.value)
-    message.value=encryptText;/* aca le asigno el al mensaje que voy a mostrar el texto ya encriptado */
-    message.style.backgroundImage="none";/* con esto hago que al llamar la funcion  desaparezca mi background */
+    message.value=encryptText;
+    message.style.backgroundImage="none";
     inputText.value="";
-    messageDoll.style.display="none";/* con esto hago que al llamar la funcion  desaparezca mi mensaje */
+    messageDoll.style.display="none";
 }
 
 function btnDesencriptarr() {
     let unencryptText=unencrypt(inputText.value)
-    message.value=unencryptText;/* aca le asigno el al mensaje que voy a mostrar el texto ya encriptado */
-    message.style.backgroundImage="none";/* con esto hago que al llamar la funcion  desaparezca mi background */
+    message.value=unencryptText;
+    message.style.backgroundImage="none";
     inputText.value="";
-    messageDoll.style.display="none";/* con esto hago que al llamar la funcion  desaparezca mi mensaje */
+    messageDoll.style.display="none";
 }
 function btnCopiar() {
-   /*  message.select(); */ /*  selecciona el mensaje al hacer click en el boton copiar */
+    message.select(); 
     navigator.clipboard.writeText(message.value);
     message.value="";
 }
